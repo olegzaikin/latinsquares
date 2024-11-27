74f0bd0c15320a317e8f05bb1840c87332e5b177
olegzaikin <oleg.zaikin@icc.ru>
2024-11-27 12:32:47 +0800

    Write squares with max orth char to file.

--- dlx_mols/dlx_mols.cpp
+++ dlx_mols/dlx_mols.cpp
@@ -15,14 +15,17 @@
 #include <iostream>
 #include <string>
 #include <sstream>
+#include <chrono>
 #include <cassert>
 
 #include "dlx_orth.h"
 
+#define time_point_t std::chrono::time_point<std::chrono::system_clock>
+
 using namespace std;
 
 string program = "dlx_mols";
-string version = "0.1.3";
+string version = "0.1.4";
 
 int strtoi(string s) {
 	assert(not s.empty());
@@ -34,6 +37,15 @@ string inttostr(int number) {
 	stringstream sstream;
 	sstream << number;
 	return sstream.str();
+}
+
+string current_time(const time_point_t &program_start) {
+    stringstream sstream;
+    const time_point_t program_end = std::chrono::system_clock::now();
+    sstream << "Elapsed : "
+    << std::chrono::duration_cast<std::chrono::seconds>(program_end - program_start).count()
+    << " seconds";
+    return sstream.str();
 }
 
 // Print a given Latin square:
@@ -103,12 +115,16 @@ int main(int argc, char *argv[])
 	string filename = argv[1];
 	const unsigned n = 10;
 
+	const time_point_t program_start = std::chrono::system_clock::now();
+
 	vector<latinsquare_t> squares = read_squares(filename, n);
 	cout << squares.size() << " Latin squares were read" << endl;
 	unsigned diagls_num = 0;
 	for (auto &square : squares) if (DLX_orth::is_diag_latinsquare(square)) diagls_num++;
 	cout << "Of them " << diagls_num << " diagonal Latin squares" << endl;
-	
+
+	set<latinsquare_t> max_orth_char_squares;
+
 	unsigned max_orth_char = 0;
 	for (int i = 0; i < squares.size(); i++) {
 		if ((i % 10000 == 0) && (i > 0)) cout << i << " squares processed" << endl;
@@ -119,15 +135,48 @@ int main(int argc, char *argv[])
 		for (unsigned j = 0; j < orth_mates.size() - 1; j++) {
 			for (unsigned j2 = j+1; j2 < orth_mates.size(); j2++) {
 				unsigned orth_char = calc_orth_char(orth_mates[j], orth_mates[j2]);
-				//if (orth_char == max_orth_char) cout << orth_char << endl;
+				// If max orth char is updated:
 				if (max_orth_char == 0 or orth_char > max_orth_char) {
 					max_orth_char = orth_char;
 					cout << "Updated max_orth_char : " << max_orth_char << endl;
 					print_square(squares[i]);
+					max_orth_char_squares.clear();
+					cout << current_time(program_start) << endl;
+				}
+				assert(orth_char <= max_orth_char);
+				// If one more squares with the current max orth char is found:
+				if (orth_char != 0 and orth_char == max_orth_char) {
+					max_orth_char_squares.insert(squares[i]);
+					cout << max_orth_char_squares.size() << " squares with orth char " << max_orth_char << endl;
+					cout << current_time(program_start) << endl;
 				}
 			}
 		}
 	}
+
+	stringstream sstream;
+	sstream << "orth_char=" << max_orth_char << "_squares_order=10";
+	string max_orth_char_squares_fname = sstream.str();
+	cout << "Writing " << max_orth_char_squares.size() << 
+		" squares to file " << max_orth_char_squares_fname << endl;
+
+	ofstream ofile(max_orth_char_squares_fname, ios_base::out);
+	for (auto &square : max_orth_char_squares) {
+		for (unsigned i = 0; i < square.size(); i++) {
+			assert(not square[i].empty());
+			for (unsigned j = 0; j < square[i].size(); j++) {
+				ofile << square[i][j] << " ";
+			}
+			ofile << endl;
+		}
+		ofile << endl;
+	}
+	ofile.close();
+
+	// max_orth_char_squares
+
+	cout << "End" << endl;
+	cout << current_time(program_start) << endl;
 
 	return 0;
 }
